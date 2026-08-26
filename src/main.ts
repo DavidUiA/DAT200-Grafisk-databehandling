6a9bcd9a73de9a8c6d0d67db5a3421dd0653d8a9
David Alexander Danielsen <gruveexp123@gmail.com>
2026-08-27 00:11:45 +0200

    add: assignment2

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,58 @@
-const canvas = document.getElementById('canvasInstructure') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-ctx.fillStyle = 'lightblue';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+{
+    const canvas = document.getElementById('canvasInstructure') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    ctx.fillStyle = 'lightblue';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.fillStyle = 'yellow';
-ctx.arc(75, 75, 50, 2 * Math.PI, 0, false)
-ctx.fill()
+    ctx.fillStyle = 'yellow';
+    ctx.arc(75, 75, 50, 2 * Math.PI, 0, false)
+    ctx.fill()
 
-ctx.fillStyle = 'black';
-ctx.beginPath();
-ctx.moveTo(100, 350);
-ctx.lineTo(150, 300);
-ctx.lineTo(450, 300);
-ctx.lineTo(500, 350);
-ctx.fill();
-ctx.closePath();
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(100, 350);
+    ctx.lineTo(150, 300);
+    ctx.lineTo(450, 300);
+    ctx.lineTo(500, 350);
+    ctx.fill();
+    ctx.closePath();
 
-ctx.fillStyle = 'green';
-ctx.beginPath();
-ctx.moveTo(150, 350);
-ctx.lineTo(450, 350);
-ctx.lineTo(450, 500);
-ctx.lineTo(150, 500);
-ctx.fill();
-ctx.closePath();
+    ctx.fillStyle = 'green';
+    ctx.beginPath();
+    ctx.moveTo(150, 350);
+    ctx.lineTo(450, 350);
+    ctx.lineTo(450, 500);
+    ctx.lineTo(150, 500);
+    ctx.fill();
+    ctx.closePath();
 
-ctx.fillStyle = 'yellow';
-for (let i = 0; i < 2; i++) {
-  ctx.beginPath();
-  ctx.moveTo(200 + 150 * i, 375);
-  ctx.lineTo(250 + 150 * i, 375);
-  ctx.lineTo(250 + 150 * i, 425);
-  ctx.lineTo(200 + 150 * i, 425);
-  ctx.fill();
-  ctx.closePath();
+    ctx.fillStyle = 'yellow';
+    for (let i = 0; i < 2; i++) {
+        ctx.beginPath();
+        ctx.moveTo(200 + 150 * i, 375);
+        ctx.lineTo(250 + 150 * i, 375);
+        ctx.lineTo(250 + 150 * i, 425);
+        ctx.lineTo(200 + 150 * i, 425);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.moveTo(275, 375);
+    ctx.lineTo(325, 375);
+    ctx.lineTo(325, 475);
+    ctx.lineTo(275, 475);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(0, 310);
+    ctx.lineTo(200, 250);
+    ctx.lineTo(300, 270);
+    ctx.lineTo(500, 220);
+    ctx.lineTo(550, 270);
+    ctx.lineTo(700, 260);
+    ctx.stroke();
 }
-
-ctx.fillStyle = 'white';
-ctx.beginPath();
-ctx.moveTo(275, 375);
-ctx.lineTo(325, 375);
-ctx.lineTo(325, 475);
-ctx.lineTo(275, 475);
-ctx.fill();
-ctx.closePath();
-
-ctx.fillStyle = 'black';
-ctx.beginPath();
-ctx.moveTo(0, 310);
-ctx.lineTo(200, 250);
-ctx.lineTo(300, 270);
-ctx.lineTo(500, 220);
-ctx.lineTo(550, 270);
-ctx.lineTo(700, 260);
-ctx.stroke();
---
+++ src/oppg2.ts
@@ -0,0 +1,77 @@
+{
+    const canvas = document.getElementById('oppg2Canvas') as HTMLCanvasElement;
+    const CIRCLE_RADIUS = 10
+
+    let strikkX = 300
+    let strikkY = 300
+    let isDragging = false;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    draw()
+
+    function draw() {
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.moveTo(150, 150);
+        ctx.lineTo(450, 150);
+        ctx.lineTo(450, 450);
+        ctx.lineTo(150, 450);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.fillStyle = 'black';
+        ctx.moveTo(150, 300);
+        ctx.lineTo(strikkX, strikkY);
+        ctx.stroke()
+        ctx.moveTo(450, 300);
+        ctx.lineTo(strikkX, strikkY);
+        ctx.stroke();
+
+        ctx.arc(strikkX, strikkY, CIRCLE_RADIUS, 0, 2*Math.PI)
+        ctx.stroke()
+    }
+
+    canvas.addEventListener("mousemove", function (e) {
+        handleMouse(e.clientX, e.clientY, false)
+        draw()
+    }, false);
+
+    function handleMouse(clientX: number, clientY: number, isClick: boolean, clickToggle: boolean = false) {
+        let mouseX = clientX - canvas.offsetLeft
+        let mouseY = clientY - canvas.offsetTop;
+        let distX = mouseX - strikkX
+        let distY = mouseY - strikkY
+        let dist = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
+
+        if (isDragging) {
+            strikkX = mouseX
+            strikkY = mouseY
+        }
+
+        if (dist < CIRCLE_RADIUS) {
+            if (isClick) {
+                if (clickToggle) {
+                    canvas.style.cursor = 'grabbing'
+                    isDragging = true
+                } else {
+                    canvas.style.cursor = 'pointer'
+                    isDragging = false
+                }
+            } else if (!isDragging) {
+                canvas.style.cursor = 'pointer'
+            }
+        } else if (!isDragging) {
+            canvas.style.cursor = 'default'
+        }
+    }
+
+    canvas.addEventListener("mousedown", function (e) {
+        handleMouse(e.clientX, e.clientY, true, true)
+    }, false);
+
+    canvas.addEventListener("mouseup", function (e) {
+        handleMouse(e.clientX, e.clientY, true, false)
+    }, false);
+}
